c8ee797a49f98ee8cda5fa395e5c789833236fcf
syjoo2304 <46588210+syjoo2304@users.noreply.github.com>
2024-12-14 17:58:44 +0900

    Update GDS.sh

--- rare_variant_association/cocoRV/GDS.sh
+++ rare_variant_association/cocoRV/GDS.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # case
-vcfAnnotated=/home/syjoo/test/cocorv/BamToBW_YUHL/sample.annotated.GT.no_chr.vcf.gz
+vcfAnnotated="[path_to_case_vcf]/sample.annotated.GT.no_chr.vcf.gz"
 Rscript ../utilities/vcf2gds.R ${vcfAnnotated} ${vcfAnnotated}.gds 4
 # control
-gnomADAnnotated="/home/syjoo/test/cocorv/KOVA/test.vcf.gz"
+gnomADAnnotated="[path_to_control_vcf]/KOVA2.vcf.gz"
 Rscript ../utilities/vcf2gds.R ${gnomADAnnotated} ${gnomADAnnotated}.gds 4
